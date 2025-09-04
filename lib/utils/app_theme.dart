d851119061ff7d82f53d9e4356d280de5e170720
Rytxxx <k.ryuto.5@icould.com>
2025-09-04 16:04:45 -0700

    fix: update CardTheme to CardThemeData and bump intl package version

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -55,7 +55,7 @@ class AppTheme {
         ),
       ),
       
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 0,
         surfaceTintColor: Colors.transparent,
         shape: RoundedRectangleBorder(
